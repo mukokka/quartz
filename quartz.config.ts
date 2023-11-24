f9bb1552856c73f91125866a31ae2f0e7808250d
mukokka <104675516+mukokka@users.noreply.github.com>
2023-11-25 00:08:12 +0800

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -9,7 +9,7 @@ const config: QuartzConfig = {
     analytics: {
       provider: "plausible",
     },
-    baseUrl: "quartz-sigma.vercel.app",
+    baseUrl: "notes.iceco.icu",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
