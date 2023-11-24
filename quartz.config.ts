886424565ad76c1caf695ee724d0aa904047aa3e
mukokka <104675516+mukokka@users.noreply.github.com>
2023-11-24 23:45:20 +0800

    url

--- quartz.config.ts
+++ quartz.config.ts
@@ -9,7 +9,7 @@ const config: QuartzConfig = {
     analytics: {
       provider: "plausible",
     },
-    baseUrl: "https://notes.iceco.icu/",
+    baseUrl: "notes.iceco.icu/",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
